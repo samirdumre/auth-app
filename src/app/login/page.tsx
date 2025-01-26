eac4f8b9b635c04b50499d236450f14b27f33f8c
Samir Dumre <samirdumre234@gmail.com>
2025-01-26 09:44:20 +0545

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex justify-center items-center text-2xl">
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-2xl">Signup</h1>
+    </div>
+  );
+}
